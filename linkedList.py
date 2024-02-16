c5272a40bfa0a50be0097a31210f5ef3901545f5
seu nome <eliasabner38@gmail.com>
2024-02-16 20:29:49 +0000

    feat:Add insert_at, insert_after_value, and remove_by_value methods to LinkedList class

--- linkedList.py
+++ linkedList.py
@@ -61,6 +61,52 @@ class LinkedList:
             itr = itr.next
             count += 1
 
+    def insert_at(self, index, data):
+        if index < 0 or index >= self.get_length():
+            raise Exception("Invalid index")
+        
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
+
+    def insert_after_value(self, data_after, data_to_insert):
+        if self.head is None:
+            return
+        if self.head.data == data_after:
+            self.head.next = Node(data_to_insert, self.head.next)
+            return
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                itr.next = Node(data_to_insert, itr.next)
+                break
+            itr = itr.next
+
+    def remove_by_value(self, data):
+        if self.head is None:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        itr = self.head
+        while itr.next:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            
+
+
+
 
 
 if __name__ == "__main__":
@@ -75,3 +121,5 @@ if __name__ == "__main__":
     print("Length of the linked list is : ", ll.get_length()) # 4
     ll.remove_at(2)
     ll.print() # banana-->mango-->orange-->
+    ll.insert_at(2, "figs")
+    ll.print() # banana-->mango-->figs-->orange-->
